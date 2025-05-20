c6c2e612ce6a7f773b50a91e448d05b01d73be70
rc2pc2 <petriccarcc@gmail.com>
2025-05-20 20:58:21 +0200

    livecoding examples

--- src/org/lessons/java/jcf/Main.java
+++ src/org/lessons/java/jcf/Main.java
@@ -1,7 +1,62 @@
 package org.lessons.java.jcf;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        
+        // ArrayList<String> stringhe = new ArrayList<>();
+        // System.out.println(stringhe.isEmpty());
+
+        // stringhe.add("Ciao");
+        // stringhe.add("Amici");
+        // stringhe.add("e");
+        // stringhe.add("Studenti");
+
+        // System.out.println(stringhe);
+
+        // System.out.println(stringhe.remove("e"));
+        // System.out.println(stringhe.isEmpty());
+
+        // for (String string : stringhe) {
+        //     System.out.println(string);
+        // }
+
+        // Set<String> invitati = new HashSet<>();
+        // invitati.add("Gina");
+        // invitati.add("Ginetto");
+        // invitati.add("Gigi");
+        // invitati.add("Gigi");
+        // invitati.add("Gigi");
+        // invitati.add("Gigi");
+        // invitati.add("Gigi");
+        // invitati.add("Gigi"); // % non aggiunge duplicati, se è già presente rimarrà quello di prima
+
+        // System.out.println(invitati.remove("Gigi"));
+        // System.out.println(invitati);
+
+        // Map<String, String> rubrica = new HashMap<>();
+        // rubrica.put("Gina", "+312354126124");
+        // rubrica.put("Ginetto", "+3942354126124");
+        // rubrica.put("Gigi", "+4417298034789");
+
+        // System.out.println(rubrica);
+        // System.out.println(rubrica.keySet());
+        // System.out.println(rubrica.values());
+        // // System.out.println(rubrica.get("Giovanni")); //& null perche' non esiste quella chiave
+
+
+        // for (String chiave : rubrica.keySet()) {
+        //     System.out.println("Chiave " + chiave + ", valore " + rubrica.get(chiave));
+        // }
+
+        // ArrayList<Integer> interi = new ArrayList<>();
+        // interi.add(22);
+        // interi.add(33);
+        // interi.add(44);
+        // System.out.println(interi);
     }
 }
